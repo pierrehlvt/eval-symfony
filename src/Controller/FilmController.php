4bf1dd97167b780e52c133046d1b6d386c9d7f0a
Pierre <pierre@halavent@gmail.com>
2021-02-12 17:20:51 +0100

    api request : add and remove on Film

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Film;
+use App\Repository\ActeurRepository;
+use App\Repository\CategorieRepository;
 use App\Repository\FilmRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -36,5 +40,63 @@ class FilmController extends AbstractController
         return $this->render('film/singleFilm.html.twig', [
            'film' => $film 
         ]);
+    }
+
+    /**
+     * @Route("/api/add/film", name="add_film", methods={"POST"})
+     *
+     * @param Request $request
+     * @param ActeurRepository $acteurRepository
+     * @param CategorieRepository $categorieRepository
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function addFilm(Request $request, ActeurRepository $acteurRepository, CategorieRepository $categorieRepository, EntityManagerInterface $manager):Response
+    {
+        $content = json_decode($request->getContent(), true);
+
+        $response = new Response();
+
+        if($content['acteurId'] && $content['nom'] && $content['categorieId']) {
+            $categorie = $categorieRepository->find($content['categorieId']);
+            $acteur = $acteurRepository->find($content['acteurId']);
+
+            $film = new Film();
+            $film->setName($content['nom'])
+                ->setCategorie($categorie)
+                ->addActeur($acteur);
+
+            $manager->persist($film);
+            $manager->flush();
+
+            $response->setStatusCode(201, "add classe ok");
+        } else {
+            $response->setStatusCode(400, "Erreur add classe");
+        }
+        return $response;
+    }
+
+    /**
+     * @Route("/api/remove/film/{id}", name="remove_film", methods={"DELETE"})
+     * 
+     * @param FilmRepository $filmRepository
+     * @param EntityManagerInterface $manager
+     * @param int $id
+     * @return Response
+     */
+    public function removeFilm(FilmRepository $filmRepository, EntityManagerInterface $manager, $id = 0): Response
+    {
+        $film = $filmRepository->find($id);
+        $response = new Response();
+
+        if($film) {
+            $manager->remove($film);
+            $manager->flush();
+            $response->setStatusCode(200);
+        } else {
+            $response->setStatusCode(400);
+        }
+        return $response;
+        
     }
 }
