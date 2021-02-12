01ef5d38ea8b58b1e1c1bfe1036dad27bee34018
Pierre <pierre@halavent@gmail.com>
2021-02-12 16:58:22 +0100

    update on security.yaml and SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login_check", name="login_check", methods={"POST"})
+     * @Route("/api/login", name="login", methods={"POST"})
+     * @param AuthenticationUtils $authenticationUtils
+     * @return JsonResponse
+     */
+    public function login(AuthenticationUtils $authenticationUtils): JsonResponse
+    {
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return new JsonResponse([$lastUsername, $error]);
+    }
+    
+    /**
+     * @Route("/api/login_check", name="login_check", methods={"POST"})
      * @return JsonResponse
      */
     public function loginCheck(): JsonResponse
